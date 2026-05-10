38c230543d5f3df97206740a467c0d7d42d79234
Kevin <renou.kevin@icloud.com>
2026-05-10 19:44:20 +0200

    add const + function + arrows et dots

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,53 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+const banner = document.querySelector("#banner")
+const arrow_left = document.querySelector(".arrow_left")
+const arrow_right = document.querySelector(".arrow_right")
+const banner_img = document.querySelector(".banner-img")
+const banner_txt = document.querySelector(".banner-txt")
+const dots = document.querySelector(".dots")
+const src_img = "./assets/images/slideshow/"
+const nbSliders = slides.length
+let i = 0
+
+// function pour changer l'image et le texte
+function updateCarrousel(i) {
+	banner_img.setAttribute("src", src_img + slides[i].image)
+	banner_txt.innerHTML = slides[i].tagLine
+}
+
+
+//fleches 
+arrow_left.addEventListener("click", () => {
+	if (i > 0){
+		i--
+	} else {
+		i = nbSliders - 1
+	}
+	updateCarrousel(i)
+	console.log(i)
+})
+
+arrow_right.addEventListener("click", () => {
+	if (i < nbSliders - 1 ) {
+		i++		
+	} else {
+		i = 0
+	}
+	updateCarrousel(i)	
+	console.log(i)
+})
+
+
+
+// dots
+for (let compter = 0; compter < slides.length; compter++){
+	let dot = document.createElement("div")
+	dots.appendChild(dot) // mettre la balise div dans la balise avec la class dots
+	dot.classList.add("dot") // ajouter la class dot
+	if (compter === 0) {
+		dot.classList.add("dot_selected")
+	}
+}
